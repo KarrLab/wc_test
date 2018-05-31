381b7d9399c7eead334243ec99c7b386f3a6d753
balazs1987 <szbazsu@gmail.com>
2018-05-31 13:17:58 -0400

    debugging

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,12 +10,12 @@ import wc_test
 import unittest
 
 class TestCore(unittest.TestCase):
-        model_path = '/tests/fixtures/min_model.xlsx'
+    model_path = 'tests/fixtures/min_model.xlsx'
 
     def test_1(self):
         tweak_specie_ids = ['ATP[c]']
         target_specie_ids = ['RNA_1[c]','RNA_2[c]','RNA_3[c]','RNA_4[c]','RNA_5[c]']
-        results = wc_test.SubmodelDynamicsTestCase().is_species_constant(model_path=model_path, end_time=600, checkpoint_period=10,
+        results = wc_test.SubmodelDynamicsTestCase().is_species_constant(model_path=self.model_path, end_time=600, checkpoint_period=10,
                                                                tweak_specie_ids=tweak_specie_ids, target_specie_ids=target_specie_ids)
 
         self.assertEqual(results, ['False', 'False', 'False', 'True', 'True'])
