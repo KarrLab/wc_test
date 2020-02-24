3e50873c65cb845864f4be8e7553da5ec93467d2
Jonathan Karr <jonrkarr@gmail.com>
2020-02-24 14:07:42 -0500

    updating for consistency with semantic versioning

--- docs/conf.py
+++ docs/conf.py
@@ -59,9 +59,7 @@ author = u'Karr Lab'
 # built documents.
 #
 # The short X.Y version.
-filename = os.path.join(os.path.dirname(__file__), '..', 'wc_test', 'VERSION')
-with open(filename, 'r') as file:
-    version = file.read()
+from wc_test._version import __version__ as version
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- wc_test/__init__.py
+++ wc_test/__init__.py
@@ -2,5 +2,4 @@ import pkg_resources
 from .core import KnowledgeBaseTestCase, ModelTestCase, SimulationTestCase
 
 # read version
-with open(pkg_resources.resource_filename('wc_test', 'VERSION'), 'r') as file:
-    __version__ = file.read().strip()
+from ._version import __version__
